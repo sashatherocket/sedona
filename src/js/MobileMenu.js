597464f72443fc83759e69f261599be5eb07e0a6
sashatherocket <35545113+sashatherocket@users.noreply.github.com>
2018-06-04 23:14:49 +0300

    Completed styling the tablet version of index.html

--- src/js/MobileMenu.js
+++ src/js/MobileMenu.js
@@ -4,17 +4,19 @@ import $ from "jquery";
 
 class MobileMenu {
   constructor() {
-    this.menuContent = $(".mobile-menu");
+    this.menuContent = $(".primary-nav");
     this.menuIcon = $(".site-header__menu-icon");
     this.events();
   }
 
   events() {
     this.menuIcon.click(this.toggleTheMenu.bind(this));
+    if($(window).width() >= 700) this.menuContent.toggleClass("primary-nav--hidden");
+
   }
 
   toggleTheMenu() {
-    this.menuContent.toggleClass("mobile-menu--hidden");
+    this.menuContent.toggleClass("primary-nav--hidden");
   }
 }
 
